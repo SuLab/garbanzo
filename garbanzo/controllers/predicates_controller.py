3684d1337b8a90b92cba66339347782870a903ef
Greg Stupp <gstupp@scripps.edu>
2017-11-20 11:19:17 -0800

    implement #9

--- garbanzo/controllers/predicates_controller.py
+++ garbanzo/controllers/predicates_controller.py
@@ -1,9 +1,6 @@
-import connexion
 from garbanzo.models.predicate import Predicate
-from datetime import date, datetime
-from typing import List, Dict
-from six import iteritems
-from ..util import deserialize_date, deserialize_datetime
+from garbanzo.utils import execute_sparql_query
+from typing import List
 
 
 def get_predicates():
@@ -13,4 +10,27 @@ def get_predicates():
 
     :rtype: List[Predicate]
     """
-    return []
+    # Possible types: {'CommonsMedia', 'Time', 'Quantity', 'WikibaseProperty', 'WikibaseItem', 'GlobeCoordinate',
+    # 'String', 'ExternalId', 'Math', 'Monolingualtext', 'TabularData', 'Url', 'GeoShape'}
+    query = """SELECT ?p ?pt ?pLabel ?d ?aliases WHERE {
+      {
+        SELECT ?p ?pt ?d (GROUP_CONCAT(DISTINCT ?alias; separator="|") as ?aliases) WHERE {
+          ?p wikibase:propertyType ?pt .
+          OPTIONAL {?p skos:altLabel ?alias FILTER (LANG (?alias) = "en")}
+          OPTIONAL {?p schema:description ?d FILTER (LANG (?d) = "en") .}
+        } GROUP BY ?p ?pt ?d
+      }
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }"""
+    results = execute_sparql_query(query)['results']['bindings']
+    results = [{k: v['value'] for k, v in item.items()} for item in results]
+    print(results[0])
+
+    items = [{'id': "wd:" + x['p'].split("/")[-1],
+              'name': x['pLabel'],
+              'definition': x.get("d", ""),
+              'aliases': x['aliases'].split("|") if x['aliases'] else [],
+              'ptype': x['pt'].replace("http://wikiba.se/ontology#", "")} for x in results]
+    # note: 'aliases' and 'ptype' are not in the official spec!
+
+    return items
